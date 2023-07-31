4396da8aeb5f7a01d89c0ca835dd8becea60e5b9
LilJade09 <josue2duran@hotmail.com>
2023-07-31 17:01:05 -0600

    login

--- MyPet_FrontEnd/src/app/app-routing.module.ts
+++ MyPet_FrontEnd/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './index/index.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/', pathMatch: 'full'},
-  {path: '', component: IndexComponent}
+  {path: '', component: IndexComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
